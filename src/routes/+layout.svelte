6112ff841f674ea8eb852396cc2302c3f2f25bf6
axel-rock <rockaxel@gmail.com>
2025-09-17 05:07:46 +0200

    feat: Basic project scaffolding (2 pages, menu, footer with hackathon's sponsor in use)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,65 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<header>
+	<h1>
+		<a href="/">ModlGuessr</a>
+	</h1>
+	<nav>
+		<a href="/leaderboard">Leaderboard</a>
+	</nav>
+</header>
+
+<main>
+	{@render children?.()}
+</main>
+
+<footer>
+	<div class="tools">
+		<span>Powered by:</span>
+		<a href="https://svelte.dev/" target="_blank">
+			<img src="/logo/svelte.svg" alt="Svelte" />
+			<span>SvelteKit</span></a
+		>
+		<a href="https://www.convex.dev/" target="_blank">
+			<img src="/logo/convex.svg" alt="Convex" />
+			<span>Convex</span></a
+		>
+		<a href="https://www.better-auth.com/" target="_blank">
+			<img src="/logo/better-auth.svg" alt="Better Auth" />
+			<span>Better Auth</span></a
+		>
+		<a href="https://useautumn.com/" target="_blank">
+			<img src="/logo/autumn.png" alt="Autumn" />
+			<span>Autumn</span></a
+		>
+	</div>
+</footer>
+
+<style>
+	.tools {
+		display: grid;
+		grid-template-columns: auto 1fr;
+		gap: 0.25rem;
+		align-items: center;
+		font-size: 0.85rem;
+
+		& > span {
+			grid-column: 1 / -1;
+		}
+
+		a {
+			display: grid;
+			grid-column: 1 / -1;
+			grid-template-columns: subgrid;
+			align-items: center;
+			font-size: 0.85rem;
+
+			img {
+				width: 2rem;
+				height: 2rem;
+				object-fit: contain;
+			}
+		}
+	}
+</style>
