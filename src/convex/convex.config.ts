71d4af1dfc78778e113fd5b7eb8be6c40b828d22
axel-rock <rockaxel@gmail.com>
2025-09-23 21:40:53 +0200

    fix: Remove reference from unused package

--- src/convex/convex.config.ts
+++ src/convex/convex.config.ts
@@ -1,10 +1,8 @@
 import { defineApp } from 'convex/server'
 import betterAuth from '@convex-dev/better-auth/convex.config'
 import autumn from '@useautumn/convex/convex.config'
-import persistentTextStreaming from '@convex-dev/persistent-text-streaming/convex.config'
 
 const app = defineApp()
 app.use(betterAuth)
 app.use(autumn)
-app.use(persistentTextStreaming)
 export default app
