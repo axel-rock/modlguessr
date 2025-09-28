c54a54c9887ae1a1edf5a6ea7a79e9e1e39e91bb
axel-rock <rockaxel@gmail.com>
2025-09-29 01:35:05 +0200

    fix: Reset constants after demo

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,3 @@
 // Globally shared constants
-
-// export const MAX_ROUNDS = 7
-// export const DURATION = 150
-export const MAX_ROUNDS = 4
-export const DURATION = 60
+export const MAX_ROUNDS = 7
+export const DURATION = 150
