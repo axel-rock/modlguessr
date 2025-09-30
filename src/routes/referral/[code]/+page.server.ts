9417386d5251125a2dfd8558cb6c56cb70f59406
axel-rock <rockaxel@gmail.com>
2025-09-30 22:34:35 +0200

    fix: Update referral redirections

--- src/routes/referral/[code]/+page.server.ts
+++ src/routes/referral/[code]/+page.server.ts
@@ -2,7 +2,7 @@ import { api } from '$convex/api'
 import { redirect } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
-export const load = (async ({ params: { code }, locals: { convex }, url, cookies }) => {
+export const load = (async ({ params: { code }, locals: { convex }, cookies }) => {
 	cookies.set('referral_code', code, { path: '/' })
 	const user = await convex.query(api.auth.getCurrentUser, {})
 
@@ -15,6 +15,6 @@ export const load = (async ({ params: { code }, locals: { convex }, url, cookies
 
 	redirect(
 		302,
-		`/login?message=${encodeURIComponent('We got your referral code! Sign up to get 5 free extra tickets!')}&redirect=${url.href}`
+		`/login?message=${encodeURIComponent('We got your referral code! Sign up to get 5 free extra tickets!')}`
 	)
 }) satisfies PageServerLoad
